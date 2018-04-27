c490af7fd01777d2b0e82b2633188dde33933706
LeonCF <1037299165@qq.com>
2018-04-27 17:43:46 +0800

    基本效果完成（未性能优化）

--- countdown.js
+++ countdown.js
@@ -5,7 +5,11 @@ var MARGIN_TOP=60;
 var MARGIN_LEFT=30;
 
 const endTime = new Date(2018,3,28,18,47,52);// js中月份特殊，0-11表示1-12月
-var curShowTimeSeconds=0;
+var curShowTimeSeconds=0
+var balls=[];
+const colors=["#33B3E5","#0099CC","#AA66CC","#9933CC","#99CC00","#669900","#FFBB33","#FF8800","#FF4444","#000000"]
+
+
 window.onload=function(){
     var canvas=document.getElementById('canvas');
     var context=canvas.getContext("2d");
@@ -22,6 +26,8 @@ window.onload=function(){
         50
     );
 }
+
+
 function getCurrentShowTImeSeconds(){  //计算倒计时时间
     var curTime=new Date();
     var ret=endTime.getTime()-curTime.getTime();
@@ -43,9 +49,61 @@ function update(){
     var curSeconds=curShowTimeSeconds%60;
 
     if(nextSeconds!=curSeconds){
+        if(parseInt(curHours/10)!=parseInt(nextHours/10)){    //小时的十位数
+            addBalls(MARGIN_LEFT+0,MARGIN_TOP,parseInt(nextHours/10));
+        }
+        if(parseInt(curHours%10)!=parseInt(nextHours%10)){    //小时的个位数
+            addBalls(MARGIN_LEFT+15*(RADIUS+1),MARGIN_TOP,parseInt(nextHours%10));
+        }
+        if(parseInt(curMinutes/10)!=parseInt(nextMinutes/10)){    //分钟的十位数
+            addBalls(MARGIN_LEFT+39*(RADIUS+1),MARGIN_TOP,parseInt(nextMinutes/10));
+        }
+        if(parseInt(curMinutes%10)!=parseInt(nextMinutes%10)){    //分钟的个位数
+            addBalls(MARGIN_LEFT+54*(RADIUS+1),MARGIN_TOP,parseInt(nextMinutes%10));
+        }
+        if(parseInt(curSeconds/10)!=parseInt(nextSeconds/10)){    //秒钟的十位数
+            addBalls(MARGIN_LEFT+78*(RADIUS+1),MARGIN_TOP,parseInt(nextSeconds/10));
+        }
+        if(parseInt(curSeconds%10)!=parseInt(nextSeconds%10)){    //秒钟的个位数
+            addBalls(MARGIN_LEFT+93*(RADIUS+1),MARGIN_TOP,parseInt(nextSeconds%10));
+        }
         curShowTimeSeconds=nextShowTimeSeconds;
     }
+
+
+    updateBalls();
 }
+function updateBalls(){
+    for(var i=0;i<balls.length;i++){
+        balls[i].x+=balls[i].vx;
+        balls[i].y+=balls[i].vy;
+        balls[i].vy+=balls[i].g;
+
+        if(balls[i].y>=WINDOW_HEIGHT-RADIUS){
+            balls[i].y=WINDOW_HEIGHT-RADIUS;
+            balls[i].vy=-balls[i].vy*0.75;
+        }
+    }
+}
+function addBalls(x,y,num){
+    for(var i=0;i<digit[num].length;i++){
+        for(var j=0;j<digit[num][i].length;j++){
+            if(digit[num][i][j]==1){
+                var aBall={
+                    x:x+j*2*(RADIUS+1)+(RADIUS+1),
+                    y:y+i*2*(RADIUS+1)+(RADIUS+1),
+                    g:1.5+Math.random(),
+                    vx:Math.pow(-1,Math.ceil(Math.random()*1000))*4,
+                    vy:-5,
+                    color:colors[Math.floor(Math.random()*colors.length)]
+                }
+
+                balls.push(aBall);
+            }
+        }
+    }
+}
+
 function render(cxt){                     //主要渲染静态页面函数
     cxt.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);//防止render重叠，对整个页面进行刷新
     var hours=parseInt(curShowTimeSeconds/3600);
@@ -54,12 +112,22 @@ function render(cxt){                     //主要渲染静态页面函数
 
     renderDigit( MARGIN_LEFT , MARGIN_TOP , parseInt(hours/10) , cxt )
     renderDigit( MARGIN_LEFT + 15*(RADIUS+1) , MARGIN_TOP , parseInt(hours%10) , cxt )
-    renderDigit( MARGIN_LEFT + 30*(RADIUS + 1) , MARGIN_TOP , 10 , cxt )
+    renderDigit( MARGIN_LEFT + 30*(RADIUS+1) , MARGIN_TOP , 10 , cxt )
     renderDigit( MARGIN_LEFT + 39*(RADIUS+1) , MARGIN_TOP , parseInt(minutes/10) , cxt);
     renderDigit( MARGIN_LEFT + 54*(RADIUS+1) , MARGIN_TOP , parseInt(minutes%10) , cxt);
     renderDigit( MARGIN_LEFT + 69*(RADIUS+1) , MARGIN_TOP , 10 , cxt);
     renderDigit( MARGIN_LEFT + 78*(RADIUS+1) , MARGIN_TOP , parseInt(seconds/10) , cxt);
     renderDigit( MARGIN_LEFT + 93*(RADIUS+1) , MARGIN_TOP , parseInt(seconds%10) , cxt);
+
+    for(var i=0;i<balls.length;i++){
+        cxt.fillStyle=balls[i].color;
+
+        cxt.beginPath();
+        cxt.arc(balls[i].x,balls[i].y,RADIUS,0,2*Math.PI,true);
+        cxt.closePath();
+
+        cxt.fill();
+    }
 }
 
 function renderDigit(x,y,num,cxt){
